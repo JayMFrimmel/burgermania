9a27af32cf4366f4378d829b71ee4706d13b20b4
JayMFrimmel <jayfrimmel@yahoo.com>
2016-11-05 11:16:20 -0400

    first commit

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,10 @@ USE burgers_db;
 
 CREATE table burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int AUTO_INCREMENT NOT NULL,
     burger_name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    TIMESTAMP,
+    TIMESTAMP()
     );
     
     
